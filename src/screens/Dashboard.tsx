f32f318aea73d4e10463b132f8fb81ad82154015
MrPinabutter <vitorafael.r.c@gmail.com>
2021-12-18 15:21:52 -0300

    first commit

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function Dashboard() {
+    
+  return (
+    <View>
+      <Text>
+
+      </Text>
+    </View>
+  )
+}
